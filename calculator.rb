6990e9a754c2d503672dd88f8d172cb2f73d361a
Princess Irabor <irabor.princess1@gmail.com>
2019-06-25 19:20:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,9 @@
 # Add your variables here
 first_number = l 
 second_number = 5
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
 
 
